f8cd4c6c0eeb174aa69231a649e9ff1b79b23db7
Rony Silva <ronysilvati@live.com>
2020-02-29 16:46:54 -0300

    Evosuite testes

--- evosuite-tests/org/apache/ibatis/datasource/DataSourceException_ESTest.java
+++ evosuite-tests/org/apache/ibatis/datasource/DataSourceException_ESTest.java
@@ -15,6 +15,16 @@ import org.junit.runner.RunWith;
 @RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true, useJEE = true) 
 public class DataSourceException_ESTest extends DataSourceException_ESTest_scaffolding {
 
+  /**
+   * COMPREENSÍVEL: Sim
+   * DE FÁCIL CRIAÇÃO: Sim
+   * IMPORTANTE PARA O CONTEXTO: Sim
+   * ----------------------------------------------------------
+   * DESCRIÇÃO: O teste em questão verifica se a classe DataSourceException
+   * prepara a exceção corretamente a depender se é uma exceção padrão
+   * ou com mensagem personalizada.
+   * ----------------------------------------------------------
+  */
   @Test(timeout = 4000)
   public void test0()  throws Throwable  {
       DataSourceException dataSourceException0 = new DataSourceException();
@@ -22,11 +32,31 @@ public class DataSourceException_ESTest extends DataSourceException_ESTest_scaff
       assertFalse(dataSourceException1.equals((Object)dataSourceException0));
   }
 
+  /**
+   * COMPREENSÍVEL: Sim
+   * DE FÁCIL CRIAÇÃO: Sim
+   * IMPORTANTE PARA O CONTEXTO: Não
+   * ----------------------------------------------------------
+   * DESCRIÇÃO: O teste em questão verifica se a classe DataSourceException
+   * prepara a exceção corretamente, porém aparentemente nenhuma verificação
+   * adicional é efetuada.
+   * ----------------------------------------------------------
+  */
   @Test(timeout = 4000)
   public void test1()  throws Throwable  {
       DataSourceException dataSourceException0 = new DataSourceException("");
   }
 
+  /**
+   * COMPREENSÍVEL: Sim
+   * DE FÁCIL CRIAÇÃO: Sim
+   * IMPORTANTE PARA O CONTEXTO: Sim
+   * ----------------------------------------------------------
+   * DESCRIÇÃO: O teste em questão verifica se a classe DataSourceException
+   * prepara a exceção corretamente a depender se é uma exceção padrão
+   * ou com mensagem personalizada.
+   * ----------------------------------------------------------
+  */
   @Test(timeout = 4000)
   public void test2()  throws Throwable  {
       DataSourceException dataSourceException0 = new DataSourceException();
